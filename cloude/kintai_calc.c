8912dcc8308f903d7d5e6183456b3677259dd996
Ryxxxxma <chige0715@gmail.com>
2026-08-04 17:06:17 +0900

    実装レビュー完了

--- cloude/kintai_calc.c
+++ cloude/kintai_calc.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <string.h>
 
 #define MAX_NAME_LEN 32
@@ -14,7 +12,7 @@ typedef struct {
 
 double calc_late_rate(const Employee *e); /* 他の変数名と名前衝突を避けるためユニークな関数名に変更 */
 int print_report(const Employee list[], int n); /* エラー処理を実装するため、int型に変更 */
-int find_worst(const Employee list[], int n);
+int find_worst(const Employee list[], int n, double *worst_rate); /* 第3引数のポインタ経由で最大遅刻率の計算結果を格納する仕様に変更 */
 
 int main(void) {
 
@@ -36,18 +34,11 @@ int main(void) {
         goto end;
     }
 
-    worst_attendance_employee = find_worst(list, EMPLOYEE_COUNT);
+    worst_attendance_employee = find_worst(list, EMPLOYEE_COUNT, &worst_late_rate);
     if (worst_attendance_employee < 0) {
         printf("[ERROR] [%s:%d] find_worst() failed\n", __func__, __LINE__);
         goto end;
     }
-
-    worst_late_rate = calc_late_rate(&list[worst_attendance_employee]);
-    if (worst_late_rate < 0.0) {
-        printf("[ERROR] [%s:%d] calc_late_rate() failed\n", __func__, __LINE__);
-        goto end;
-    }
-    worst_late_rate *= 100; /* 割合をパーセント表示にするため 100 を掛ける */
 
     printf("要注意社員: %s (遅刻率 %.1f%%)\n", list[worst_attendance_employee].name, worst_late_rate);
 
@@ -63,6 +54,10 @@ double calc_late_rate(const Employee *e) {
 
     if (e == NULL) {
         printf("[ERROR] [%s:%d] argument \"e\" is NULL\n", __func__, __LINE__);
+        return late_rate;
+    }
+    if (e->work_days <= 0) {
+        printf("[ERROR] [%s:%d] employee:%s has %d days of attendance\n", __func__, __LINE__, e->name, e->work_days);
         return late_rate;
     }
 
@@ -99,11 +94,12 @@ int print_report(const Employee list[], int n) {
     return ret;
 }
 
-int find_worst(const Employee list[], int n) {
+int find_worst(const Employee list[], int n, double *worst_rate) {
     int i = 0;
     int worst_employee = -1;
     double late_rate = 0.0;
-    double worst_rate = 0.0;
+
+    *worst_rate = -1.0; /* 遅刻率は0.0以上のため、初回で必ず更新される */
 
     if (list == NULL) {
         printf("[ERROR] [%s:%d] argument \"list\" is NULL\n", __func__, __LINE__);
@@ -116,11 +112,12 @@ int find_worst(const Employee list[], int n) {
             printf("[ERROR] [%s:%d] calc_late_rate() failed\n", __func__, __LINE__);
             return -1;
         }
-        if (worst_rate < late_rate) {
+        if (*worst_rate < late_rate) { /* 遅刻率が同率の場合、インデックスが最小の社員を返す */
             worst_employee = i;
-            worst_rate = late_rate;
+            *worst_rate = late_rate;
         }
     }
+    *worst_rate *= 100; /* 割合をパーセント表示にするため 100 を掛ける */
 
     return worst_employee;
 }
